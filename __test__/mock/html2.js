ce57248b36ec751c0ac887dfb6c6db3882f89ee2
huozhi <gilesliu12@gmail.com>
2017-08-03 01:43:21 +0800

    complex html testing

---
+++ __test__/mock/html2.js
@@ -0,0 +1,36 @@
+module.exports = `
+<div>
+  <p>
+    <br>
+  </p>
+  <p style="text-align: center; font-size: 15px;" data-mce-style="text-align: center; font-size: 15px;">
+    <img src="https://www.tinymce.com/images/glyph-tinymce@2x.png" alt="TinyMCE" width="110" height="97" data-mce-src="images/glyph-tinymce@2x.png">
+  </p>
+  <p style="text-align: center; color: #666; font-size: 15px; font-family: avenir; font-weight: 200;" data-mce-style="text-align: center; color: #666; font-size: 15px; font-family: avenir; font-weight: 200;">
+    This demo includes these TinyMCE Premium
+    <span class="mce-spellchecker-word" data-mce-bogus="1" data-mce-word="Plugins" data-mce-index="0">
+      Plugins
+    </span>:
+    <a href="https://www.tinymce.com/powerpaste" data-mce-href="powerpaste" data-mce-linkchecker-status="valid">
+      PowerPaste
+    </a>
+     (improved Word paste),
+     <a href="https://www.tinymce.com/accessibility-checker" data-mce-href="accessibility-checker" data-mce-linkchecker-status="valid">Accessibility Checker</a>, <a href="https://www.tinymce.com/spell-checker-pro" data-mce-href="spell-checker-pro"
+      data-mce-linkchecker-status="valid">Spell Checker Pro
+    </a>
+    &amp; file management with
+    <a href="http://www.moxiemanager.com" data-mce-href="http://www.moxiemanager.com" data-mce-linkchecker-status="valid">
+      MoxieManager
+    </a>
+    .
+  </p>
+  <p style="text-align: center; color: #666; font-size: 15px; font-family: avenir; font-weight: 200;"
+    data-mce-style="text-align: center; color: #666; font-size: 15px; font-family: avenir; font-weight: 200;">TinyMCE is a platform indepen<strong>dent web-</strong>based JavaScript HTML WYSIWYG
+    <br> editor control released as open source <em>under</em> LGPL.
+  </p>
+  <p style="text-align: center; color: #666; font-size: 15px; font-family: avenir; font-weight: 200;" data-mce-style="text-align: center; color: #666; font-size: 15px; font-family: avenir; font-weight: 200;"><em>TinyMCE enables you to convert HTML <span class="mce-spellchecker-word" data-mce-bogus="1" data-mce-word="textarea" data-mce-index="0">textarea</span> fields or other HTML elements to editor instances.</em></p>
+  <p>
+    <br>
+  </p>
+</div>
+`
--- __test__/tokenizer.spec.js
+++ __test__/tokenizer.spec.js
@@ -1,6 +1,11 @@
 const tokenizer = require('../src/tokenizer')
 const html1 = require('./mock/html1')
+const html2 = require('./mock/html2')
 
 it('tokenizer parse correctly', () => {
   expect(tokenizer(html1)).toMatchSnapshot()
+})
+
+it('tokenizer parse correctly', () => {
+  expect(tokenizer(html2)).toMatchSnapshot()
 })
