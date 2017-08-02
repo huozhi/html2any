880d1ebf362f8e3a0dec4009df82e107cab99c20
huozhi <gilesliu12@gmail.com>
2017-08-03 01:43:10 +0800

    manually tokenizer insteadof regex

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -39,12 +39,59 @@ function getTokenFromTag(tag) {
   }
 }
 
+function makeToken(tag) {
+  const isTag = tag.startsWith('<') && tag.endsWith('>')
+
+  if (!isTag) {
+    return {
+      type: 'string',
+      value: tag,
+    }
+  } else if (tag.startsWith('</')) {
+    return {
+      type: 'end',
+      name: tag.slice(2, -1),
+    }
+  } else {
+    const match = tag.match(/<(\w+)\s*([^/>]*)/)
+    return {
+      type: utils.isSelfClose(match[1]) ? 'self-close' : 'start',
+      name: match[1],
+      attribues: getAttributes(match[2]),
+    }
+  }
+}
+
+function splitTokens(html) {
+  let i = 0
+  let j = 0
+  const tokens = []
+  while (i < html.length) {
+    const curr = html[i]
+    if (curr === '<') {
+      if (j < i) {
+        tokens.push(html.slice(j, i))
+        j = i
+      }
+      let k = i
+      while (html[k] !== '>') k++
+      tokens.push(html.slice(i, k + 1))
+      i = j = k + 1
+      continue
+    }
+    i++
+  }
+  return tokens
+}
+
 function tokenizer(html) {
-  return html.split(/(?=<\/?\w+\s*[^>]*>)/)
-    .map(s => s.replace(/\n/g, ''))
+  // return html.split(/(?=<[^>]*>)/)
+  return splitTokens(html)
+    .map(s => s.replace(/^\n|\n$/g, ''))
     .map(s => s.trim())
     .filter(Boolean)
-    .map(getTokenFromTag)
+    .map(makeToken)
+    // .map(getTokenFromTag)
 }
 
 module.exports = tokenizer
