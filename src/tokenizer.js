ea1cf1c94bbfcfa5ddcdd33b66a2294194f5b3af
huozhi <gilesliu12@gmail.com>
2017-08-10 22:47:34 +0800

    fix missing attributes after slash

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -16,29 +16,6 @@ function getAttributes(str) {
   }, {})
 }
 
-function getTokenFromTag(tag) {
-  const isEndTag = tag.startsWith('</')
-  if (isEndTag) {
-    return {
-      type: 'end',
-      name: tag.slice(2, -1),
-    }
-  } else {
-    const hasChildrenText = tag[tag.length - 1] !== '>'
-    const closePos = hasChildrenText ? tag.indexOf('>') : tag.length - 1
-    const tagSelf = tag.slice(0, closePos) // `<div class="3">123` to `<div class="3"`
-    const children = hasChildrenText ? tag.slice(closePos + 1).trim() : null
-    const match = tagSelf.match(/<(\w+)\s*([^/>]*)/)
-
-    return {
-      type: utils.isSelfClose(match[1]) ? 'self-close' : 'start',
-      children,
-      name: match[1],
-      attribues: getAttributes(match[2]),
-    }
-  }
-}
-
 function makeToken(tag) {
   const isTag = tag.startsWith('<') && tag.endsWith('>')
 
@@ -53,11 +30,11 @@ function makeToken(tag) {
       name: tag.slice(2, -1),
     }
   } else {
-    const match = tag.match(/<(\w+)\s*([^/>]*)/)
+    const match = tag.match(/<(\w+)\s*([^>]*)/)
     return {
       type: utils.isSelfClose(match[1]) ? 'self-close' : 'start',
       name: match[1],
-      attribues: getAttributes(match[2]),
+      attribues: getAttributes(match[2].slice(0, -1)),
     }
   }
 }
@@ -85,13 +62,11 @@ function splitTokens(html) {
 }
 
 function tokenizer(html) {
-  // return html.split(/(?=<[^>]*>)/)
   return splitTokens(html)
     .map(s => s.replace(/^\n|\n$/g, ''))
     .map(s => s.trim())
     .filter(Boolean)
     .map(makeToken)
-    // .map(getTokenFromTag)
 }
 
 module.exports = tokenizer
